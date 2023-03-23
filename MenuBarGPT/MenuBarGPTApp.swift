372696130f2592c07f02d50f9839f84a21f037d6
Viacheslav Shkliarov <vch.shklyarov@gmail.com>
2023-03-23 21:08:29 +0200

    Initial Commit

---
+++ MenuBarGPT/MenuBarGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuBarGPTApp.swift
+//  MenuBarGPT
+//
+//  Created by Viacheslav Shkliarov on 23.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct MenuBarGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
