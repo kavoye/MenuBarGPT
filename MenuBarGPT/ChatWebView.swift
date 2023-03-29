49c825b05531b8b72c2069f9511f47bf303d626a
Viacheslav Shkliarov <vch.shklyarov@gmail.com>
2023-03-30 00:30:02 +0300

    chore: reindent file

--- MenuBarGPT/ChatWebView.swift
+++ MenuBarGPT/ChatWebView.swift
@@ -10,7 +10,7 @@ import WebKit
 
 struct ChatWebView: NSViewRepresentable {
     let view = WKWebView()
-
+    
     func makeNSView(context: Context) -> WKWebView {
         let chatURL = URL(string: "https://chat.openai.com")!
         view.load(URLRequest(url: chatURL))
@@ -19,10 +19,10 @@ struct ChatWebView: NSViewRepresentable {
         Timer.scheduledTimer(withTimeInterval: 3600, repeats: true) { _ in
             view.reload()
         }
-
+        
         return view
     }
-   
+    
     func updateNSView(_ uiView: WKWebView, context: Context) {}
- 
+    
 }
